b233668da9d9274dd8c9b76c2e3b4030987b7138
ByungKwanLee <leebk@kaist.ac.kr>
2022-02-18 14:28:33 +0900

    v0.1

--- fast_pretrain_adv.py
+++ fast_pretrain_adv.py
@@ -38,13 +38,13 @@ parser = argparse.ArgumentParser()
 # model parameter
 parser.add_argument('--dataset', default='imagenet', type=str)
 parser.add_argument('--network', default='resnet', type=str)
-parser.add_argument('--depth', default=18, type=int)
+parser.add_argument('--depth', default=50, type=int)
 parser.add_argument('--gpu', default='0,1,2,3,4', type=str)
 
 # learning parameter
 parser.add_argument('--learning_rate', default=0.1, type=float)
 parser.add_argument('--weight_decay', default=0.0002, type=float)
-parser.add_argument('--batch_size', default=1024, type=float)
+parser.add_argument('--batch_size', default=512, type=float)
 parser.add_argument('--test_batch_size', default=128, type=float)
 parser.add_argument('--epoch', default=60, type=int)
 
