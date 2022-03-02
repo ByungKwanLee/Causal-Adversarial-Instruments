061f73cb770a5e0f7a623098a85fa94cc9f27d37
ByungKwanLee <leebk@kaist.ac.kr>
2022-03-02 16:34:50 +0900

    v0.1

--- fast_pretrain_adv.py
+++ fast_pretrain_adv.py
@@ -35,17 +35,17 @@ parser = argparse.ArgumentParser()
 
 # model parameter
 parser.add_argument('--dataset', default='cifar10', type=str)
-parser.add_argument('--network', default='resnet', type=str)
+parser.add_argument('--network', default='vgg', type=str)
 
-parser.add_argument('--depth', default=18, type=int)
-parser.add_argument('--gpu', default='0,1,2,3,4', type=str)
+parser.add_argument('--depth', default=16, type=int)
+parser.add_argument('--gpu', default='0', type=str)
 parser.add_argument('--pretrained', default=False, type=str2bool)
 
 # learning parameter
 parser.add_argument('--learning_rate', default=0.1, type=float)
 parser.add_argument('--weight_decay', default=0.0002, type=float)
-parser.add_argument('--batch_size', default=512, type=float)
-parser.add_argument('--test_batch_size', default=128, type=float)
+parser.add_argument('--batch_size', default=128, type=float)
+parser.add_argument('--test_batch_size', default=256, type=float)
 parser.add_argument('--epoch', default=60, type=int)
 
 # attack parameter
