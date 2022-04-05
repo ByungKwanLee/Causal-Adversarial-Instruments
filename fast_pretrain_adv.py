418b483cf3991ecbc45795d54480cf1b633d04b6
DooDooKim <arkimjh@kaist.ac.kr>
2022-04-05 22:27:29 +0900

    v0.1

--- fast_pretrain_adv.py
+++ fast_pretrain_adv.py
@@ -28,11 +28,11 @@ torch.autograd.profiler.profile(False)
 parser = argparse.ArgumentParser()
 
 # model parameter
-parser.add_argument('--dataset', default='imagenet', type=str)
+parser.add_argument('--dataset', default='cifar100', type=str)
 parser.add_argument('--network', default='vgg', type=str)
 parser.add_argument('--depth', default=16, type=int)
-parser.add_argument('--gpu', default='0,1,2,3,4', type=str)
-parser.add_argument('--port', default='12355', type=str)
+parser.add_argument('--gpu', default='0,1,2,3', type=str)
+parser.add_argument('--port', default='12356', type=str)
 parser.add_argument('--pretrained', default=False, type=str2bool) # True for loading ImageNet pre-trained model
 
 # learning parameter
