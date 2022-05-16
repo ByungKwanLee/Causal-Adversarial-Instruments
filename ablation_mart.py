f470cedc343012807b8a6463a5e1958fcd0c4f53
ByungKwanLee <leebk@kaist.ac.kr>
2022-05-17 03:06:06 +0900

    v0.1

--- ablation_mart.py
+++ ablation_mart.py
@@ -211,7 +211,7 @@ def mart_loss(logits,
     true_probs = torch.gather(nat_probs, 1, (targets.unsqueeze(1)).long()).squeeze()
     loss_robust = (1.0 / logits.shape[0]) * torch.sum(
         torch.sum(kl(torch.log(adv_probs + 1e-12), nat_probs), dim=1) * (1.0000001 - true_probs))
-    loss = loss_adv + float(3) * loss_robust
+    loss = loss_adv + float(1) * loss_robust
     return loss
 
 def main_worker(rank, ngpus_per_node=ngpus_per_node):
